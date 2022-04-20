d7cb36cd571511284530807b5c478d27288580e5
asepdera <90121160+asepdera@users.noreply.github.com>
2022-04-20 16:52:45 +0700

    update FilmController
    manggil model film nya dua kali jadi asep hapus satu

--- app/Http/Controllers/API/FilmController.php
+++ app/Http/Controllers/API/FilmController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\Film;
 use Illuminate\Http\Request;
-use App\Models\Film;
 use App\Models\FilmDetails;
 
 class FilmController extends Controller
